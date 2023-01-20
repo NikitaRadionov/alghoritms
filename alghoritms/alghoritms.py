5eecefc72434a359811a567ff826ef338830c83d
Nikita <72099127+NikitaRadionov@users.noreply.github.com>
2023-01-20 19:16:42 +0300

    Add files via upload

--- alghoritms/alghoritms.py
+++ alghoritms/alghoritms.py
@@ -332,60 +332,6 @@ def bubleSort_optimized(lst:list, reverse:bool=False, copy:bool=True) -> list:
     return a
 
 
-def heapsort(lst:list, reverse:bool=False) -> list:
-    """
-        This is algorithm of sorting by heap.
-        Ascending sort by default.
-        Set reverse=True, if you want descending order
-
-    Complexity: O(nlog(n)) where n = len(lst)
-
-    Args:
-        lst (list): unsorted list
-        reverse (bool, optional): ascending/descending order. Defaults to False.
-
-    Returns:
-        list: sorted list
-    """
-
-    def maxheapify(a:list, i:int, heap_size:int):
-        L = 2*i + 1
-        R = 2*i + 2
-        largest = L if L < heap_size and a[L] > a[i] else i
-        largest = R if R < heap_size and a[R] > a[largest] else largest
-        if largest != i:
-            swap = a[largest]
-            a[largest] = a[i]
-            a[i] = swap
-            maxheapify(a, largest, heap_size)
-
-    def minheapify(a:list, i:int, heap_size:int):
-        L = 2*i + 1
-        R = 2*i + 2
-        minest = L if L < heap_size and a[L] < a[i] else i
-        minest = R if R < heap_size and a[R] < a[minest] else minest
-        if minest != i:
-            swap = a[minest]
-            a[minest] = a[i]
-            a[i] = swap
-            maxheapify(a, minest, heap_size)
-
-    a = Heap(lst, max_heap=(not reverse)).A
-
-    size = len(a)
-    for i in range(len(a) - 1, 0, -1):
-        swap = a[0]
-        a[0] = a[i]
-        a[i] = swap
-        size -= 1
-        if reverse:
-            minheapify(a, 0, size)
-        else:
-            maxheapify(a, 0, size)
-    return a
-
-
-
 # string algorithms
 def prefix_function_naive(s:str) -> list:
     """
@@ -1179,6 +1125,59 @@ class QuadraticPolynomial(Poly):
             if D < 0:
                 return []
 
+
+#sorting
+def heapsort(lst:list, reverse:bool=False) -> list:
+    """
+        This is algorithm of sorting by heap.
+        Ascending sort by default.
+        Set reverse=True, if you want descending order
+
+    Complexity: O(nlog(n)) where n = len(lst)
+
+    Args:
+        lst (list): unsorted list
+        reverse (bool, optional): ascending/descending order. Defaults to False.
+
+    Returns:
+        list: sorted list
+    """
+
+    def maxheapify(a:list, i:int, heap_size:int):
+        L = 2*i + 1
+        R = 2*i + 2
+        largest = L if L < heap_size and a[L] > a[i] else i
+        largest = R if R < heap_size and a[R] > a[largest] else largest
+        if largest != i:
+            swap = a[largest]
+            a[largest] = a[i]
+            a[i] = swap
+            maxheapify(a, largest, heap_size)
+
+    def minheapify(a:list, i:int, heap_size:int):
+        L = 2*i + 1
+        R = 2*i + 2
+        minest = L if L < heap_size and a[L] < a[i] else i
+        minest = R if R < heap_size and a[R] < a[minest] else minest
+        if minest != i:
+            swap = a[minest]
+            a[minest] = a[i]
+            a[i] = swap
+            maxheapify(a, minest, heap_size)
+
+    a = Heap(lst, max_heap=(not reverse)).A
+
+    size = len(a)
+    for i in range(len(a) - 1, 0, -1):
+        swap = a[0]
+        a[0] = a[i]
+        a[i] = swap
+        size -= 1
+        if reverse:
+            minheapify(a, 0, size)
+        else:
+            maxheapify(a, 0, size)
+    return a
 
 
 if __name__ == "__main__":
